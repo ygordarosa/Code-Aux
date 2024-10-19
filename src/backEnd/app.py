c00c1ada34dd8a6fb68ab159399a6d923518ec45
Ygor da Rosa <yygorrosa@gmail.com>
2024-10-19 15:03:00 -0300

    feat: adicionado pasta rotas

--- src/backEnd/app.py
+++ src/backEnd/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify, request
-
+from routes.get import getFolders
 app = Flask(__name__)
 
 @app.route("/get", methods=['GET'])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    response = getFolders()
+    return response
 
 
 
---
+++ src/backEnd/routes/get.py
@@ -0,0 +1,10 @@
+
+
+
+
+def getFolders():
+    print("pegou os folders")
+
+    return {
+        "folders":{"tome":"tome"}
+    }
